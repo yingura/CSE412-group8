1af84f908db146cfcc9700463d10547a484e1ed3
Gina <gina_brasen@cox.net>
2022-12-03 19:53:07 -0700

    Update artist.sql

--- SQL-code/artist.sql
+++ SQL-code/artist.sql
@@ -1,7 +1,7 @@
-INSERT INTO artist(artist_name,profile_picture) VALUES ('BTS','https://github.com/yingura/CSE412-group8/blob/main/profile_picture/bts.jpg');
-INSERT INTO artist(artist_name,profile_picture) VALUES ('Gen Hoshino','https://github.com/yingura/CSE412-group8/blob/main/profile_picture/gen%20hoshino.jpg');
-INSERT INTO artist(artist_name,profile_picture) VALUES ('Taylor Swift','https://github.com/yingura/CSE412-group8/blob/main/profile_picture/taylor%20swift.jpg');
-INSERT INTO artist(artist_name,profile_picture) VALUES ('JAY-Z','https://github.com/yingura/CSE412-group8/blob/main/profile_picture/jay-z.jpg');
-INSERT INTO artist(artist_name,profile_picture) VALUES ('Linkin Park','https://github.com/yingura/CSE412-group8/blob/main/profile_picture/linkin%20park.jpg');
-INSERT INTO artist(artist_name,profile_picture) VALUES ('YOASOBI','https://github.com/yingura/CSE412-group8/blob/main/profile_picture/yoasobi.jpg');
-INSERT INTO artist(artist_name,profile_picture) VALUES ('ARASHI','https://github.com/yingura/CSE412-group8/blob/main/profile_picture/arashi.jpg');
+INSERT INTO artist(artist_name,profile_picture) VALUES ('BTS','https://raw.githubusercontent.com/yingura/CSE412-group8/main/profile_picture/bts.jpg');
+INSERT INTO artist(artist_name,profile_picture) VALUES ('Gen Hoshino','https://raw.githubusercontent.com/yingura/CSE412-group8/main/profile_picture/gen%20hoshino.jpg');
+INSERT INTO artist(artist_name,profile_picture) VALUES ('Taylor Swift','https://raw.githubusercontent.com/yingura/CSE412-group8/main/profile_picture/taylor%20swift.jpg');
+INSERT INTO artist(artist_name,profile_picture) VALUES ('JAY-Z','https://raw.githubusercontent.com/yingura/CSE412-group8/main/profile_picture/jay-z.jpg');
+INSERT INTO artist(artist_name,profile_picture) VALUES ('Linkin Park','https://raw.githubusercontent.com/yingura/CSE412-group8/main/profile_picture/linkin%20park.jpg');
+INSERT INTO artist(artist_name,profile_picture) VALUES ('YOASOBI','https://raw.githubusercontent.com/yingura/CSE412-group8/main/profile_picture/yoasobi.jpg');
+INSERT INTO artist(artist_name,profile_picture) VALUES ('ARASHI','https://raw.githubusercontent.com/yingura/CSE412-group8/main/profile_picture/arashi.jpg');
